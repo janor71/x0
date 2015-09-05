1104e1616272cb7bcaf30966ff9f806696a0d290
Rebeca <reca985@yahoo.com.ar>
2015-09-05 12:34:27 -0400

    changes were made to circle

--- circles0.java
+++ circles0.java
@@ -2,10 +2,13 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(400, 400);                 // increase window size
+   background(250,84,0);          // add color 
 }
 
-// Next frame:  circle at (x,y). //
+// Next frame:  circle at (x,y).  //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 200,200,80,80 );       // move the circle to the middle 
+  ellipse(200,200,40,40);         //add another circle inside
+  ellipse(200,200,20,20);         // add a 3rd circle inside 
 }
